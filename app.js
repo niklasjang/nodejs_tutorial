369bd826ddef5e02650638d6a7758795a2a70b3d
Niklas Jang <niklasjang@gmail.com>
2019-08-18 17:43:15 +0900

    After trim code

--- app.js
+++ app.js
@@ -7,31 +7,35 @@ app.set('views','./views');
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/topic/:id', function(req,res){
-    var id = req.params.id;
-    fs.readdir('data', function(err, files){
-        if(err){ 
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
-        fs.readFile('data/'+id, 'utf8', function(err,data){
-            if(err){
-                console.log(err);
-                res.status(500).send('Internal Server Error');
-            }
-            res.render('view', {topics : files, title: id, description:data});
-        });
-    });
-});
-
-app.get('/topic', function(req,res){
+app.get('/topic/new', function(req, res){
     fs.readdir('data', function(err, files){
         if(err){ 
             console.log(err);
             res.status(500).send('/topic Internal Server Error');
         }
-        //files에는 dir에 있는 파일들의 이름이 각각 배열로 담겨있다.
-        res.render('view', {topics:files});
+        res.render('new', {topics :files});
+    });
+});
+
+app.get(['/topic', '/topic/:id'], function(req,res){
+    var id = req.params.id;
+    fs.readdir('data', function(err, files){
+        if(err){ 
+            console.log(err);
+            res.status(500).send('/topic Internal Server Error');
+        }
+        if(id){//id값이 있을 때
+            fs.readFile('data/'+id, 'utf8', function(err,data){
+                if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                }
+                res.render('view', {topics : files, title: id, description:data});
+            });
+        }else{//id값이 없을 때
+            //files에는 dir에 있는 파일들의 이름이 각각 배열로 담겨있다.
+                res.render('view', {topics : files, title: 'Welcome', description:'Hello javascript'});
+        }
     });
 });
 
@@ -45,15 +49,12 @@ app.post('/topic', function(req, res){
             console.log(err);
             res.status(500).send('/topic Internal Server Error');
         }else{
-            res.send('Success');
+            res.redirect('/topic/'+title);
         }
     });
 });
 
-app.get('/topic/new', function(req, res){
-    console.log('topic/new is ok');
-    res.render('new');
-});
+
 
 app.get('/', function(req, res){
     res.send('hi');
